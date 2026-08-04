1089afd5ca9be2a21c2000ee24c607fd47905032
controldeprocesostr <controldeprocesostr@gmail.com>
2026-08-04 18:56:52 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-// erp-prensas-v663-rechazos-fix10-a1a3a4-dinamico
-/* Service worker sin caché pesado. */
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => { event.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request))); });
